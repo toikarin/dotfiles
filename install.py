e042d24c034563ec0471a1cf30d4b64e29859855
Tomi Oikarinen <tomi.oikarinen@iki.fi>
2012-01-16 12:49:50 +0200

    fix empty line printing

--- install.py
+++ install.py
@@ -9,9 +9,9 @@ script_dir = os.path.dirname(os.path.realpath(__file__))
 i = installer.create_installer_from_parser_opts(script_dir)
 
 if i.dry_run:
-    print()
+    print("")
     print("Doing dry-run. Nothing is installed. Use -n flag to do real installation if everything looks ok.")
-    print()
+    print("")
 
 i.create_symlink(".gitconfig")
 i.create_symlink(".git-excludes")
